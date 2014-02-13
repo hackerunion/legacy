e349e8af1c2cd7a5beb8f6947dafee2e70333a93
Jarrett Cruger <jcrugzz@gmail.com>
2014-02-12 21:22:19 -0500

    [refactor] clean up file loading in main route because OCD

--- root.js
+++ root.js
@@ -1,6 +1,7 @@
 /* global require */
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var path = require('path');
 var app = express();
@@ -45,14 +46,18 @@ function chapter(req, res) {
 function chapter_directory(req, res) {
 
   function callback(err, files) {
-    var data = [];
-
-    for (var i=0; i<files.length; i++) {
-      try {
-        var d = require('./chapters/' + req.params.chapter_name + '/' + req.params.directory + '/' + files[i]);
-        data.push(d)
-      } catch(exception) {}
-    }
+    //
+    // Validate the files are json before we try and load them
+    //
+    var root = path.join(__dirname, 'chapters', req.params.chapter_name, req.params.directory);
+    var data  = files.filter(function (file) {
+      return /.json$/.test(file);
+    }).map(function (file) {
+      var d;
+      try { d = require(path.join(root, file)) }
+      catch (ex) { d = null }
+      return d;
+    }).filter(Boolean);
 
     res.json(200, data);
   }
