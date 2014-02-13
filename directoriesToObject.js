70c65ea18cbf316097ccd00d090c88d4f58986cb
Jarrett Cruger <jcrugzz@gmail.com>
2014-02-12 22:26:12 -0500

    [fix] use correct api calls

--- directoriesToObject.js
+++ directoriesToObject.js
@@ -14,13 +14,13 @@ var path = require('path');
 //
 var chaptersDir = path.join(__dirname, 'chapters');
 
-var chapters = fs.readDirSync(chaptersDir);
+var chapters = fs.readdirSync(chaptersDir);
 
 module.exports = chapters.reduce(function (acc, chapter) {
   var chapterDir = path.join(chaptersDir, chapter);
-  acc[chapter] = fs.readDirSync(chapterDir).reduce(function (paths, resource) {
+  acc[chapter] = fs.readdirSync(chapterDir).reduce(function (paths, resource) {
     var resourceDir = path.join(chapterDir, resource);
-    paths[resource] = fs.readDirSync(resourceDir).map(function(file) {
+    paths[resource] = fs.readdirSync(resourceDir).map(function(file) {
       return path.join(resourceDir, file);
     });
     return paths;
