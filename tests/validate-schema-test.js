f946b496b2b29de7531cb7c4c1b2f70e774fc3f5
Jarrett Cruger <jcrugzz@gmail.com>
2014-02-12 22:25:55 -0500

    [fix tests] scrap old tests and add frame for new tests

---
+++ tests/validate-schema-test.js
@@ -0,0 +1,10 @@
+var test = require('tape');
+var Joi = require('joi');
+
+var data = require('../directoriesToObject');
+
+test('validate the resources', function (t) {
+  t.plan(1);
+
+  t.ok(true);
+});
