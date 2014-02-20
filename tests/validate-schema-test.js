1dc9a3518ca45441b9f187b894eadd7482c2150a
Jarrett Cruger <jcrugzz@gmail.com>
2014-02-19 21:18:27 -0500

    [test] first try at test

--- tests/validate-schema-test.js
+++ tests/validate-schema-test.js
@@ -1,10 +1,19 @@
 var test = require('tape');
 var Joi = require('joi');
 
-var data = require('../directoriesToObject');
+var chapters = require('../chapters-obj');
+var userSchema = require('./user-schema');
 
-test('validate the resources', function (t) {
-  t.plan(1);
+test('validate all the users based on defined schema', function (t) {
 
-  t.ok(true);
+  Object.keys(chapters).forEach(function (chapter) {
+    Object.keys(chapters[chapter]).forEach(function (resource) {
+      if (resource === 'users') {
+        chapters[chapter][resource].forEach(function (obj) {
+          t.equals(Joi.validate(obj, userSchema), null, 'User object is valid');
+        });
+      }
+    });
+  });
+  t.end();
 });
