2727fb533a4c8ca19b219c476262d122d29e73a1
Jarrett Cruger <jcrugzz@gmail.com>
2014-02-19 21:38:57 -0500

    [tests] OMG TESTS WORK

--- tests/user-schema.js
+++ tests/user-schema.js
@@ -2,9 +2,14 @@ var Joi = require('joi');
 
 module.exports = {
   email: Joi.string().email().required(),
-  name: Joi.string().alphanum().required(),
+  name: Joi.string().required(),
   skills: Joi.array().required(),
-  twitter: Joi.string().required(),
+  twitter: Joi.string(),
   github: Joi.string().required(),
-  projects: Joi.array().required()
+  projects: Joi.array(),
+  v: Joi.number(),
+  //
+  // TODO: REGEX THAT ITS A VALID URL OKAY
+  //
+  url: Joi.string()
 };
--- tests/validate-schema-test.js
+++ tests/validate-schema-test.js
@@ -5,7 +5,6 @@ var chapters = require('../chapters-obj');
 var userSchema = require('./user-schema');
 
 test('validate all the users based on defined schema', function (t) {
-
   Object.keys(chapters).forEach(function (chapter) {
     Object.keys(chapters[chapter]).forEach(function (resource) {
       if (resource === 'users') {
